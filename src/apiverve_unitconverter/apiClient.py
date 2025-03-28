a77c4286701cb397de35dffdb56a6a9dd8f63f8b
APIVerve Team <hello@apiverve.com>
2025-03-28 18:00:13 -0500

    Bug Fixes

--- src/apiverve_unitconverter/apiClient.py
+++ src/apiverve_unitconverter/apiClient.py
@@ -10,10 +10,10 @@ class UnitconverterAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/unitconverter'
+        self.base_url = 'https://api.apiverve.com/v1/unitconverter'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
